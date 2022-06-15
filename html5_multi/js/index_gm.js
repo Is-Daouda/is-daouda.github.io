03afbfc3fa38b09ce43e2173a6ad969c14eb888d
Is Daouda <isdaouda.n@gmail.com>
2022-06-15 00:39:30 +0000

    update

---
+++ html5_multi/js/index_gm.js
@@ -0,0 +1,111 @@
+var canvas = document.getElementById('canvas');
+var updateSize = 0;
+var initGame = 0;
+var normalScreen = (window.innerWidth > window.innerHeight);
+var rscLink = "";
+
+// --- I Can Transform v2.5 >>>
+document.documentElement.className = "loading_page";
+document.body.className = "loading_page";
+
+var langIndex = 0; // English
+var userLang = navigator.language || navigator.userLanguage;
+if (userLang === "fr" || userLang === "fr-FR" || userLang === "fr-fr") langIndex = 1;
+
+var paramRscLink = "https://is-daouda.github.io/html5_multi/";
+var isJsParam1, isJsParam2, isJsParam3;
+
+function loadObjDesc() {
+	var txtFile = new XMLHttpRequest();
+	var allParam = "";
+	txtFile.onreadystatechange = function () {
+		if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
+			allParam = txtFile.responseText;
+			var paramList = allParam.split('\n');
+			
+			document.getElementById('loading_title').innerHTML = paramList[langIndex];
+			document.getElementById('loading_msg').innerHTML = paramList[2 + langIndex];
+			document.getElementById('msg_start').innerHTML = paramList[4 + langIndex];
+			isJsParam1 = paramList[6];
+			isJsParam2 = paramList[7];
+			isJsParam3 = paramList[8];
+		}
+	}
+	txtFile.open("GET", paramRscLink + "param.txt", true);
+	txtFile.send(null);		
+}
+
+loadObjDesc();
+
+function hideLoadingScreen() {
+	document.getElementById("screen_loading").remove();
+	document.documentElement.className = "game_page";
+	document.body.className = "game_page";
+	document.getElementById('screen_cover').style.display = "block";
+}
+// <<< I Can Transform v2.5 ---
+
+function onResize() {
+	if (normalScreen) {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		updateSize = 1;			
+	}
+}
+
+window.addEventListener("resize", onResize, true);
+
+function removeCover(event) {
+   if (initGame === 1) {
+		document.getElementById('screen_cover').removeEventListener("click", removeCover);
+		document.getElementById('screen_cover').remove();
+		initGame = 2;
+	}
+}
+
+document.getElementById('screen_cover').addEventListener("click", removeCover);
+
+window.addEventListener("load", function() {
+	window.focus();
+	document.body.addEventListener("click", function(e) {
+		window.focus();
+	}, false);
+});
+
+window.Module = {
+	preRun: [],
+	postRun: [],
+	canvas: canvas,
+	onRuntimeInitialized: function() {
+		isJsShowGameAds();
+		hideLoadingScreen();
+		
+		for(ms of [0, 100, 1000, 3000]) {
+			window.setTimeout(onResize, ms);
+		}
+	},
+	print: console.log,
+	printErr: console.error,
+};
+
+Module['locateFile'] = function(path, prefix) {
+  if (path.endsWith(".data")) return rscLink + "isengine.data";
+  return prefix + path;
+}
+
+function loadScriptAsync(src) {
+	var s, r, t;
+	r = false;
+	s = document.createElement("script");
+	s.type = "text/javascript";
+	s.src = src;
+	s.onload = s.onreadystatechange = function() {
+		if (!r && (!this.readyState || this.readyState === "complete")) {
+			r = true;
+		}
+	};
+	t = document.getElementsByTagName('script')[0];
+	t.parentNode.insertBefore(s, t);
+}
+
+loadScriptAsync(rscLink + "isengine.js");

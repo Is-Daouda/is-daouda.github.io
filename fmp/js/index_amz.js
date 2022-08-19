7506f2ce0c4622b27f7db77c9e95cd2a9934d12a
Is Daouda <isdaouda.n@gmail.com>
2022-08-19 03:11:31 +0000

    Update index_amz.js

--- fmp/js/index_amz.js
+++ fmp/js/index_amz.js
@@ -197,7 +197,7 @@ var isJsPlayers = {};
 
 var isJsGameLevel = 0;
 var isJsCrossWorld = 0;
-var isJsRoomStep = 0;
+//var isJsRoomStep = 0;
 var isJsAvoidChangeRoom = 0;
 var isJsMultiPlayerStarted = 0;
 var canLockRoom = true;

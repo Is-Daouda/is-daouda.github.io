cc6fa28e15c8c1c94395f7e52f7000b1dfcae949
Is Daouda <45235532+Is-Daouda@users.noreply.github.com>
2023-01-01 10:08:41 +0000

    Add files via upload

--- html5_multi/js/index_multi.js
+++ html5_multi/js/index_multi.js
@@ -4,31 +4,7 @@ var isJsGameState = 2;
 var isJsInitGame = 0;
 var showError = 0;
 var landscapeMode = false;
-var rscLink = "https://is-daouda.github.io/html5_multi/";
-
-var sdkState = 0;
-window.SDK_OPTIONS = {
-	gameId: "3pve1sz9o9lpu8bbzi0ab73bbytb1gub",
-	onEvent: function (a) {
-		switch (a.name) {
-		case "SDK_GAME_PAUSE": isJsGameState = 0; break;
-		case "SDK_GAME_START": isJsGameState = 1; break;
-		case "SDK_READY": sdkState = 1; break;
-		}
-	}
-};
-
-(function (a, b, c) {
-	var d = a.getElementsByTagName(b)[0];
-	a.getElementById(c) || (a = a.createElement(b), 
-	a.id = c, 
-	a.src = "https://api.gamemonetize.com/sdk.js", 
-	d.parentNode.insertBefore(a, d))
-})(document, "script", "gamemonetize-sdk");
-
-function isJsShowGameAds() {
-	if (typeof sdk !== 'undefined' && sdk.showBanner !== 'undefined') sdk.showBanner();
-}
+var rscLink = "";
 
 // --- I Can Transform v2.5 >>>
 document.documentElement.className = "loading_page";

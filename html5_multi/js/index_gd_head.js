58d19b10b85f7cbbc5644ba6762102f846430db0
Is Daouda <45235532+Is-Daouda@users.noreply.github.com>
2023-12-27 13:40:24 +0000

    Add files via upload

---
+++ html5_multi/js/index_gd_head.js
@@ -0,0 +1,36 @@
+var isJsGameState = 2;
+var sdkState = 0;
+
+// init SDK
+window["GD_OPTIONS"] = {
+    "gameId": "d3c66467f11247f084fd4d201e043ad3",
+    "onEvent": function(event) {
+        switch (event.name) {
+            case "SDK_GAME_START":
+                isJsGameState = 1;
+                break;
+            case "SDK_GAME_PAUSE":
+                isJsGameState = 0;
+                break;
+            case "SDK_READY":
+                sdkState = 1;
+                break;
+        }
+    },
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://html5.api.gamedistribution.com/main.min.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'gamedistribution-jssdk'));
+
+function isJsShowGameAds() {
+    if (typeof gdsdk !== 'undefined' && gdsdk.showAd !== 'undefined') {
+    //     gdsdk.showAd();
+    alert("Show Ad!");
+    }
+}
